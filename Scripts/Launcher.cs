789a125b17d05c51526a9446e9c36914753becdc
duy5399 <58471974+duy5399@users.noreply.github.com>
2019-12-10 22:55:01 +0700

    Add files via upload

---
+++ Scripts/Launcher.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.UI;
+using System.Text;
+
+public class Launcher : MonoBehaviourPunCallbacks
+{
+    public GameObject roomListScreen;
+    // Start is called before the first frame update
+    void Start()
+    {
+        roomListScreen = GameObject.Find("Canvas").transform.Find("ConnectedSceen").transform.Find("RoomList").gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public GameObject loadingScreen;
+    public GameObject connectedScreen;
+    public GameObject disconnectedScreen;
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinLobby(TypedLobby.Default); //khi kết nối thành công tới Photon Server (Master) thì sẽ kết nối vào Lobby (sảnh chờ) 
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        loadingScreen.SetActive(false);
+        disconnectedScreen.SetActive(true); //không kết nỗi bởi lí do gì thì hiện màn hình disconnect
+    }
+
+    public override void OnJoinedLobby() //hàm được gọi khi dòng PhotonNetwork.JoinLobby(TypedLobby.Default); thực hiện thành công
+    {
+        if(loadingScreen.activeSelf)        //activeSelf: đang kích hoạt
+            loadingScreen.SetActive(false);
+        if (disconnectedScreen.activeSelf)  
+            disconnectedScreen.SetActive(false);
+        
+        connectedScreen.SetActive(true);    //màn hình chính Lobby sẽ có Create Room và Join Room
+    }
+
+    public override void OnRoomListUpdate(List<RoomInfo> roomList)
+    {
+        var list = new StringBuilder();
+        base.OnRoomListUpdate(roomList);
+        foreach(var room in roomList)
+        {
+            list.Append("Name: " + room.Name + "\t\t" + room.PlayerCount + "/" + room.MaxPlayers + " player");
+        }
+        roomListScreen.transform.Find("Text").GetComponent<Text>().text = "Room List" + "\n" + list.ToString() + "\n";
+    }
+
+}
---
+++ Scripts/UIhandler.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class UIhandler : MonoBehaviourPunCallbacks
+{
+    public GameObject alertQuit;
+    public InputField createRoomTxt;
+    public InputField joinRoomTxt;
+    public Button createBtn;
+    public Button joinBtn;
+
+    public void OnClick_ExitBtn()   //nút Quit Game
+    {
+        alertQuit.SetActive(true);
+    }
+
+    public void OnClick_NoBtn()     //nút No
+    {
+        alertQuit.SetActive(false);
+    }
+
+    public void OnClick_YesBtn()    //nút Yes
+    {
+        Application.Quit();
+    }
+
+    public void OnClick_ReconnectBtn()
+    {
+        PhotonNetwork.ConnectUsingSettings(); //khi nhấn Connect_button sẽ kết nối tới Photon Server theo App ID tại PhotonServerSettings
+        PhotonNetwork.LoadLevel(1);
+    }
+
+    public void OnChange_CreateNameRoom()    //hàm gọi khi giá trị ở ô Text Box nhập tên phòng thay đổi (tạo phòng)
+    {
+        if (createRoomTxt.text.Length > 0)    //kiểm tra tên nhập vào lớn hơn 2 kí tự
+        {
+            createBtn.interactable = true; //button CreateRoomBtn sẽ enable //interactable: tương tác
+        }
+        else
+            createBtn.interactable = false;
+    }
+
+    public void OnChange_JoinNameRoom()    //hàm gọi khi giá trị ở ô Text Box nhập tên phòng thay đổi (tạo phòng)
+    {
+        if (joinRoomTxt.text.Length > 0)    //kiểm tra tên nhập vào lớn hơn 2 kí tự
+        {
+            joinBtn.interactable = true; //button CreateRoomBtn sẽ enable //interactable: tương tác
+        }
+        else
+            joinBtn.interactable = false;
+    }
+
+    public void OnClick_CreateRoom()    //xảy ra khi nhấn Create Room Button
+    {
+        PhotonNetwork.CreateRoom(createRoomTxt.text, new RoomOptions { MaxPlayers = 10 }, null);  //tạo phòng mới với tên nhập ở TextBox với số lượng người chới tối đa là 10    
+    }
+
+    public void OnClick_JoinRoom()  //xảy ra khi nhấn Join Room Button
+    {
+        PhotonNetwork.JoinRoom(joinRoomTxt.text, null); //tham gia phòng với tên nhập ở TextBox
+    }
+
+    public override void OnJoinedRoom()
+    {
+        //print("Room Joned Sucess");
+        PhotonNetwork.LoadLevel(2); //load tới SampleScene
+    }
+
+    public override void OnJoinRoomFailed(short returnCode, string message)
+    {
+        print("Room Failed " + returnCode + " Message " + message); //hàm check báo JoinRoomFailed khi chạy thử game
+    }
+    
+}
